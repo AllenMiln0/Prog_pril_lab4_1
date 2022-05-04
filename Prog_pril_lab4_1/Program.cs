72bc2b71480098ac6843356c16c8c39380cf07b2
Анастасия Жукова <nastya@myttk.ru>
2022-05-05 04:58:20 +0700

    end

--- Prog_pril_lab4_1/Program.cs
+++ Prog_pril_lab4_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,24 +14,41 @@ namespace Prog_pril_lab4_1
             //Console.Write("Введите кол-во студентов: ");
             //int n = int.Parse(Console.ReadLine());
             RealNumbers xyz = new RealNumbers();
-            
-            xyz.input();
-            xyz.output();
+
+            int choise = 0;
+
+            bool temp = true;
+            while (temp)
+            {
+                Console.WriteLine("Выберите способ ввода: 1- чтение из файла, 2 - ввод вручную");
+                
+                choise = Convert.ToInt32(Console.ReadLine());
+                if (choise == 1 || choise == 2) temp = false;
+                if (temp) Console.WriteLine("Введите снова.");
+            }
+
+            if (choise == 1)
+            {
+                xyz.diskOut();
+            }
+            else xyz.diskIn();
+           /* xyz.diskIn();
+            xyz.diskOut(); 
             
             //объект, инициализированный конструктором без параметров
             RealNumbers st1 = new RealNumbers();
             Console.WriteLine("Инициализация конструктором без параметров: "); 
-            st1.output();
+            st1.diskOut();
             
             //объект, инициализированный конструктором с параметрами
             RealNumbers st2 = new RealNumbers(0.0001, 2, 345);
             Console.WriteLine("Инициализация конструктором c параметрами: ");
             st2.setY(0.03);
-            st2.output();
+            st2.diskOut();
             Console.ReadKey(true);
 
+            */
             
-
         }
     }
 }
--- Prog_pril_lab4_1/RealNumbers.cs
+++ Prog_pril_lab4_1/RealNumbers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Prog_pril_lab4_1
 {
@@ -29,21 +30,40 @@ namespace Prog_pril_lab4_1
         public void setY(double x1) { y = x1; }
         public void setZ(double x1) { z = x1; }
 
-        public void input()
+        public void diskIn()/*пользовательские данные в файл*/
         {
+            StreamWriter F = new StreamWriter("test.txt", false);
             Console.Write("Введите Х: "); 
             x = Convert.ToDouble(Console.ReadLine());
             Console.Write("Введите Y: ");
             y = Convert.ToDouble(Console.ReadLine());
             Console.Write("Введите Z: ");
             z = Convert.ToDouble(Console.ReadLine());
-        }
-        public void output()
-        {
-            Console.WriteLine("X\t\tY\t\tZ\tCреднее геометрическое   Количество цифр суммы");
-
+            string str_f1, str_f2;
+            str_f1 = x.ToString() + " " + y.ToString() + " " + z.ToString();
+            str_f2 = Convert.ToString(sr_geom()) + " " + Convert.ToString(count_digit());
             string str = String.Format("{0,-15:F6}{1,-15:F6}{2, -15:F6}{3, -20:F6}{4}\n", x, y, z, sr_geom(), count_digit());
             Console.WriteLine(str);
+            F.WriteLine(str_f1);
+            F.WriteLine(str_f2);
+            F.Close();
+        }
+        
+        public void diskOut()/*чтение из файла*/
+        {
+
+            StreamReader F = new StreamReader("test.txt");
+            string data;
+            data = F.ReadLine();
+            string [] str2 = data.Split(' ');
+            x = Convert.ToDouble(str2[0]);
+            y = Convert.ToDouble(str2[1]);
+            z = Convert.ToDouble(str2[2]);
+            Console.WriteLine("X\t\tY\t\tZ\tCреднее геометрическое   Количество цифр суммы");
+            
+            string str = String.Format("{0,-15:F6}{1,-15:F6}{2, -15:F6}{3, -20:F6}{4}\n", x, y, z, sr_geom(), count_digit());
+            Console.WriteLine(str);
+            F.Close();
             
         }
         public double sr_geom()
